889c2f2e919d83015ed48718e20030521f893f7c
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-01-12 11:03:11 +0100

    completed number challenges

--- 2. numbers.js
+++ 2. numbers.js
@@ -27,14 +27,32 @@ function generateMulitiplicationTable(number) {
   return numbers;
 }
 
-console.log(generateMulitiplicationTable(5));
+//console.log(generateMulitiplicationTable(5));
 
 // 3. Write a Javascript function to return wether a value is divisible by a certain number
 // 	isDivisible(333,7)
 // 	=> false
 
+function isDivisible(number, divideBy){
+  return number % divideBy === 0;
+}
+
+//console.log(isDivisible(8,3));
+
 // 4. Write a Javascript function that return an array with even numbers between a range
 // 	getEvenNumbersInRange(56,1211)
+
+function getEvenNumbersInRange(startRange,endRange){
+  const evenNumbers = [];
+  for (let i = startRange; i < endRange; i++){
+    if (i % 2 === 0){
+      evenNumbers.push(i);
+    }
+  }
+  return evenNumbers;
+}
+
+// console.log(getEvenNumbersInRange(3,200))
 
 // 5. Write a Javascript function that calculate the distance between two coordinates.
 // 	getDistance([20,100],[50,800])
